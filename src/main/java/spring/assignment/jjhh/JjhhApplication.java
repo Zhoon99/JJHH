5b0374da11d5b20b3ced045ecbde123de030cbd4
Zhoon99 <chjh827@gmail.com>
2022-11-18 16:40:41 +0900

    Initial commit

---
+++ src/main/java/spring/assignment/jjhh/JjhhApplication.java
@@ -0,0 +1,13 @@
+package spring.assignment.jjhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjhhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjhhApplication.class, args);
+	}
+
+}
