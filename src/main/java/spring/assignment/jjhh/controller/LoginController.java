187f3764d1069f141432e8aa16d97ce2370bc33d
pjh141 <82190627+pjh141@users.noreply.github.com>
2022-12-06 02:10:17 +0900

    프로필 수정 끝

--- src/main/java/spring/assignment/jjhh/controller/LoginController.java
+++ src/main/java/spring/assignment/jjhh/controller/LoginController.java
@@ -7,12 +7,16 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import spring.assignment.jjhh.dto.AccountDto;
+import spring.assignment.jjhh.dto.Pro_ImageDto;
 import spring.assignment.jjhh.entity.Account;
 import spring.assignment.jjhh.repository.AccountRepository;
+import spring.assignment.jjhh.service.ProfileService;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +27,8 @@ public class LoginController {
     
     @Autowired
 	private AccountRepository accountRepository;
+    
+    private final ProfileService profileService;
     
     @GetMapping("/login")
     public String index() {
@@ -43,14 +49,23 @@ public class LoginController {
 
 	@PostMapping("/new")
     public String memberForm(AccountDto dto, 
-            BindingResult bindingResult, Model model) {
-        log.info("====================>" + dto );
-        
+            BindingResult bindingResult,
+            @RequestParam("imgsw") MultipartFile file,   Model model) {
+       
+       System.out.println("file : " + file); 
         if(bindingResult.hasErrors()) {
             return "member/memberForm";
         }
         
-        try {            
+        try {
+        	Pro_ImageDto img = profileService.ImageSave(file);
+        	
+//        	String imgurl = "/assets/img/data/" + img.getFileOriName();
+        	String imgurl = "/images/" + img.getFileOriName();
+        	System.out.println(imgurl);
+        	dto.setProfile_img(imgurl);
+        	dto.setProfileOriName(img.getFileOriName());
+        	dto.setRole("basic");
             Account createMember = Account.createAccount(dto, passwordEncoder);
             accountRepository.save(createMember);
         } catch(IllegalStateException e) {
--- src/main/java/spring/assignment/jjhh/controller/ProfileController.java
+++ src/main/java/spring/assignment/jjhh/controller/ProfileController.java
@@ -2,16 +2,24 @@ package spring.assignment.jjhh.controller;
 
 import java.security.Principal;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import spring.assignment.jjhh.dto.Pro_ImageDto;
 import spring.assignment.jjhh.entity.Account;
+import spring.assignment.jjhh.repository.AccountRepository;
+import spring.assignment.jjhh.service.PrincipalDatails;
 import spring.assignment.jjhh.service.ProfileService;
 
 @Controller
@@ -21,22 +29,61 @@ public class ProfileController {
 	
 	private final ProfileService profileService;
 	
+	private final AccountRepository accountRepository;
+	
+	private final PasswordEncoder passwordEncoder;
+
 	@GetMapping("/user/profile")
-	public String profile(Model model) {
-		Long id = (long) 12; 
-		model.addAttribute("data_id",id);
+	public String my_profile(@RequestParam("id") Long id, Model model) {
 		Account acc = profileService.selrect_Acc(id);
 		model.addAttribute("account", acc);
-		
 		return "profile";
 	}
 	
-	
+
 	@GetMapping("/user/profile/edit")
-	public String profile_edit(@RequestParam("id") Long id, Model model) {
-//		Long id = (long) 12;
-		Account acc = profileService.selrect_Acc(id);
-		model.addAttribute("account", acc);
-		return "profile_edit";
+	public String profile_edit(@RequestParam("id") Long id, 
+			@AuthenticationPrincipal PrincipalDatails principalDatails, Model model) {
+		
+		if(principalDatails.getid() == id) {
+			Account acc = profileService.selrect_Acc(id);
+			model.addAttribute("account", acc);
+			return "profile_edit";
+		}
+		else {
+			return "redirect:/user/profile?id=" + principalDatails.getid();
+		}
 	}
+	
+	@PostMapping("/user/profile/edit/{id}")
+	public String profile_update(@PathVariable(value = "id") Long id, Account acc, 
+			@RequestParam(value = "imgsw", required = false) MultipartFile file, Model model) {
+		
+		System.out.println("아이디:" + acc.getNick());
+		System.out.println("비밀번호:" + acc.getPassword());
+		String sourceFileName = file.getOriginalFilename();
+		System.out.println("이미지 : " + sourceFileName);
+		String ps = passwordEncoder.encode(acc.getPassword());
+		
+		Account ac = profileService.selrect_Acc(id);
+		ac.setNick(acc.getNick());
+		if(acc.getPassword() != null) ac.setPassword(ps);
+		
+		if(!file.isEmpty()) {
+			if(ac.getProfileImg() != null) profileService.deleteFile(ac.getProfileImg());
+			ac.setProfileImg(null);
+			ac.setProfileOriName(null);
+			
+			Pro_ImageDto img = profileService.ImageSave(file);
+			String imgurl = "/images/" + img.getFileOriName();
+        	ac.setProfileImg(imgurl);
+        	ac.setProfileOriName(img.getFileOriName());
+		}
+		
+		accountRepository.save(ac);
+		
+		//model.addAttribute("account", acc);
+		return "redirect:/";
+	}
+	
 }
