77c20100bb82658e90f3460cbc6318fa02c9c23d
Zhoon99 <chjh827@gmail.com>
2022-12-10 19:38:46 +0900

    페이징, 오류 수정

--- src/main/java/spring/assignment/jjhh/repository/PortfolioRepository.java
+++ src/main/java/spring/assignment/jjhh/repository/PortfolioRepository.java
@@ -12,7 +12,7 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     @Query("SELECT p FROM Portfolio p WHERE p.projectName LIKE %:search% AND p.disclosure = '공개'")
     Page<Portfolio> searchPublicPortfolio(String search, Pageable pageable);
 
-    @Query("SELECT p FROM Portfolio p WHERE p.account.accountId = :accountId")
+    @Query("SELECT p FROM Portfolio p WHERE p.account.accountId = :accountId ORDER BY p.regDate DESC")
     Page<Portfolio> getMyPortfolio(Long accountId, Pageable pageable);
 
     @Modifying
